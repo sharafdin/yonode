0ac8734f31899bd6f7cdd5a6c053249ee0723585
abdinasir-Tman <abdinasirmursal2@gmail.com>
2024-02-01 00:54:48 -0800

    added user entity

--- packages/yonode-templates/javascript/MongoDB/TypeORM/api-template/src/config/db.config.js
+++ packages/yonode-templates/javascript/MongoDB/TypeORM/api-template/src/config/db.config.js
@@ -1,19 +1,24 @@
-import mongoose from "mongoose";
+import "reflect-metadata";
+import { DataSource } from "typeorm";
 import { dbURL } from "./initial.config.js";
 import chalk from "chalk";
 
-const connectDB = async () => {
-    try {
-        await mongoose.connect(dbURL, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            dbName: 'yonode'
-        });
-        console.log(`${chalk.green.bold('Connected')} to the database ✅`);
-    } catch (err) {
-        console.log(`${chalk.red.bold('Error')} connecting to database`, err);
-        process.exit(1);
-    }
-};
+const AppDataSource = new DataSource({
+  type: "mongodb",
+  url: dbURL,
+  database: "yonode",
+  entities: ["./src/entity/**/*.js"],
+  synchronize: true,
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+});
 
-export default connectDB;
+AppDataSource.initialize()
+  .then(() => {
+    console.log(`${chalk.green.bold("Connected")} to the database ✅`);
+  })
+  .catch((err) => {
+    console.log(`${chalk.red.bold("Error")} connecting to database`, err);
+  });
+
+export default AppDataSource;
