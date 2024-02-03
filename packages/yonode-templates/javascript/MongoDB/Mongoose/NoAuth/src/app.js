05df26be6a9a7d4cc514318ee13d1535e4a29b7b
Mr Sharafdin <83120892+isasharafdin@users.noreply.github.com>
2024-02-03 13:52:43 +0300

    Update app.js

--- packages/yonode-templates/javascript/MongoDB/Mongoose/NoAuth/src/app.js
+++ packages/yonode-templates/javascript/MongoDB/Mongoose/NoAuth/src/app.js
@@ -3,9 +3,7 @@ import express from 'express';
 import { port } from './config/initial.config.js';
 import connectDB from './config/db.config.js';
 
-
 const PORT = port;
-
 const app = express();
 
 // database connection
