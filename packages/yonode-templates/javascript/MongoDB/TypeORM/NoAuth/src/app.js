c5da5d65cba9d54d70fcc463b1d8a576ab817ab7
abdinasir-Tman <abdinasirmursal2@gmail.com>
2024-02-03 01:34:48 -0800

    finished noAuth

--- packages/yonode-templates/javascript/MongoDB/TypeORM/NoAuth/src/app.js
+++ packages/yonode-templates/javascript/MongoDB/TypeORM/NoAuth/src/app.js
@@ -9,55 +9,20 @@ import { rateLimit } from "express-rate-limit";
 import helmet from "helmet";
 import morgan from "morgan";
 
-import userRouter from "./routes/user.router.js";
 import AppDataSource from "./config/db.config.js";
+import routerName from "./routes/router.js";
 
-AppDataSource.initialize()
-  .then(() => {
-    console.log(`${chalk.green.bold("Connected")} to the database ✅`);
-    const PORT = port;
+const PORT = port;
+const app = express();
 
-    const app = express();
+app.use(express.json());
+app.use("/api/v1/", routerName);
 
-    app.use(morgan("dev"));
-
-    var whitelist = ["http://localhost:8000"];
-
-    var corsOptionsDelegate = function (req, callback) {
-      var corsOptions;
-      if (whitelist.indexOf(req.header("Origin")) !== -1) {
-        corsOptions = { origin: true };
-      } else {
-        corsOptions = { origin: false };
-      }
-      callback(null, corsOptions);
-    };
-
-    app.use(cors(corsOptionsDelegate));
-    app.use(express.json());
-    app.use(helmet());
-    app.use(cookieParser());
-
-    const apiRateLimit = rateLimit({
-      windowMs: 15 * 60 * 1000,
-      max: 10,
-      message: "Rate limit exceeded",
-    });
-
-    app.use(apiRateLimit);
-
-    // route management
-    app.use("/api/v1/users", userRouter);
-
-    app.listen(PORT, () => {
-      console.log(process.env.PORT);
-      console.log(
-        `${chalk.green.bold("Server")} is listening on port ${chalk.green.bold(
-          PORT
-        )} 🚀`
-      );
-    });
-  })
-  .catch((err) => {
-    console.log(`${chalk.red.bold("Error")} connecting to database`, err);
-  });
+app.listen(PORT, () => {
+  console.log(process.env.PORT);
+  console.log(
+    `${chalk.green.bold("Server")} is listening on port ${chalk.green.bold(
+      PORT
+    )} 🚀`
+  );
+});
