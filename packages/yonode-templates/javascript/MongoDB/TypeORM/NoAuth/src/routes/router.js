bf7866962d844067b8c6c8a0d9a25f53c398a738
abdinasir-Tman <abdinasirmursal2@gmail.com>
2024-02-03 08:29:13 -0800

    modified router

--- packages/yonode-templates/javascript/MongoDB/TypeORM/NoAuth/src/routes/router.js
+++ packages/yonode-templates/javascript/MongoDB/TypeORM/NoAuth/src/routes/router.js
@@ -1,8 +1,5 @@
 import express from "express";
-import { controlerName } from "../controllers/controller.js";
 
 const routerName = express.Router();
-
-routerName.get("/", controlerName);
 
 export default routerName;
