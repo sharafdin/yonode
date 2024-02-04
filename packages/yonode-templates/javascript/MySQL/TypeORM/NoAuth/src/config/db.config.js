3baf64fb36f5018628e08df532a97fa3b2f8f358
abdinasir-Tman <abdinasirmursal2@gmail.com>
2024-02-04 00:51:58 -0800

    started MySQL typeorm noAuth

---
+++ packages/yonode-templates/javascript/MySQL/TypeORM/NoAuth/src/config/db.config.js
@@ -0,0 +1,20 @@
+import "reflect-metadata";
+import { DataSource } from "typeorm";
+import { dbName, dbURL } from "./initial.config.js";
+import chalk from "chalk";
+
+const AppDataSource = new DataSource({
+  type: "mongodb",
+  url: dbURL,
+  database: dbName,
+  entities: ["../entity/**/*.js"],
+  synchronize: true,
+});
+
+AppDataSource.initialize()
+  .then(async () => {
+    console.log(`${chalk.green.bold("Connected")} to the database`);
+  })
+  .catch((error) => console.log(`${chalk.red.bold("Failed")} to connect the database: ${error}`));
+
+export default AppDataSource;
