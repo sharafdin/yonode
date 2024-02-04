4e76a3230e4f17f768cd70a88d27ed9b12158a4d
miirshe <miirshe@gmail.com>
2024-02-04 18:48:43 +0300

    Completed set up of Auth and NoAuth of Mysql with Sequelize ORM

---
+++ packages/yonode-templates/javascript/MySQL/Sequelize/Auth/src/config/db.config.js
@@ -0,0 +1,17 @@
+import chalk from "chalk";
+import { Sequelize } from "sequelize"
+import { databaseHost, databaseName, databasePassword, databasePort, databaseUsername} from './initial.config.js';
+export const connectDB = () => {
+    const sequelize = new Sequelize(databaseName,databaseUsername,databasePassword,{
+        dialect : "mysql",
+        host: databaseHost,
+        port : databasePort
+    });
+    sequelize.authenticate()
+    .then(() => {
+        console.log(`${chalk.green.bold('Connection')} has been established successfully `);
+    })
+    .catch( (err) => {
+        console.error(`${chalk.red.bold('Unable')} to connect to the database`,err);
+    })
+}
