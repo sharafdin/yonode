ef17c5754bf3df09b81a56c69cc4e986570ed089
miirshe <miirshe@gmail.com>
2024-02-04 23:29:36 +0300

    completed Prisma noAuth for mysql

--- packages/yonode-templates/javascript/MySQL/Prisma/NoAuth/src/config/initial.config.js
+++ packages/yonode-templates/javascript/MySQL/Prisma/NoAuth/src/config/initial.config.js
@@ -1,10 +1,4 @@
 import dotenv from 'dotenv';
-
 dotenv.config();
 
-export const serverPort = process.env.SERVER_PORT;
-export const databaseName = process.env.DATABASE_NAME;
-export const databaseUsername = process.env.DATABASE_USERNAME;
-export const databasePassword = process.env.DATABASE_PASSWORD;
-export const databaseHost = process.env.DATABASE_HOST;
-export const databasePort = process.env.DATABASE_PORT;
+export const port = process.env.PORT || 8000;
