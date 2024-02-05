3eee24ab9cc8cf0106feea5e699c7bf09f6cd724
abdinasir-Tman <abdinasirmursal2@gmail.com>
2024-02-05 07:18:57 -0800

    restored to the default

--- packages/yonode-templates/javascript/MySQL/TypeORM/NoAuth/src/config/db.config.js
+++ packages/yonode-templates/javascript/MySQL/TypeORM/NoAuth/src/config/db.config.js
@@ -1,13 +1,21 @@
 import "reflect-metadata";
 import { DataSource } from "typeorm";
-import { dbName, dbURL } from "./initial.config.js";
+import {
+  databaseHost,
+  databaseName,
+  databasePassword,
+  databasePort,
+  databaseUsername,
+} from "./initial.config.js";
 import chalk from "chalk";
 
 const AppDataSource = new DataSource({
   type: "mysql",
-  url: dbURL,
-
-  database: dbName,
+  host: databaseHost,
+  username: databaseUsername,
+  password: databasePassword,
+  port: databasePort,
+  database: databaseName,
   entities: ["../entity/**/*.js"],
 });
 
--- packages/yonode-templates/javascript/MySQL/TypeORM/NoAuth/src/config/initial.config.js
+++ packages/yonode-templates/javascript/MySQL/TypeORM/NoAuth/src/config/initial.config.js
@@ -2,6 +2,9 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-export const port = process.env.PORT || 8000;
-export const dbURL = process.env.DATABASE_URL;
-export const dbName = process.env.DB_NAME;
+export const port = process.env.SERVER_PORT;
+export const databaseName = process.env.DATABASE_NAME;
+export const databaseUsername = process.env.DATABASE_USERNAME;
+export const databasePassword = process.env.DATABASE_PASSWORD;
+export const databaseHost = process.env.DATABASE_HOST;
+export const databasePort = process.env.DATABASE_PORT;
