3e8d2a595dd71442bd22014aef686985b38ae189
Mr Sharafdin <83120892+isasharafdin@users.noreply.github.com>
2024-02-09 07:06:17 +0300

    Update app.js

--- packages/yonode-templates/JS-MySQL-TypeORM-NoAuth/src/app.js
+++ packages/yonode-templates/JS-MySQL-TypeORM-NoAuth/src/app.js
@@ -2,12 +2,15 @@
 import express from "express";
 import chalk from "chalk";
 
+// import your files
 import { port } from "./config/initial.config.js";
 import "./config/db.config.js";
 
+// Initializing the app
 const app = express();
 
 // rest of your code here
+
 
 app.listen(port, () => {
   console.log(`${chalk.green.bold("Server")} is listening on port ${port}`);
