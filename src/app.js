1739eee92920006d803da64a07682593677aa551
isasharafdin <isasharafdin@gmail.com>
2024-02-09 18:28:21 +0300

    prepared the branch template

--- src/app.js
+++ src/app.js
@@ -1,14 +1,16 @@
 // import the packages
 import express from "express";
-
-//import your files
-import { port } from "./config/initial.config.js";
 import chalk from "chalk";
+
+// import your files
+import { port } from "./config/initial.config.js";
+import "./config/db.config.js";
 
 // Initializing the app
 const app = express();
 
 // rest of your code here
+
 
 app.listen(port, () => {
   console.log(`${chalk.green.bold("Server")} is listening on port ${port}`);
