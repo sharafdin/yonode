aef7a4a205d3dfe2f5ec69c5f4bd0802c1f7f2f8
abdinasir-Tman <abdinasirmursal2@gmail.com>
2024-02-08 20:31:34 -0800

    fixed mongoDb typeorm notAuth

--- packages/yonode-templates/JS-MongoDB-TypeORM-NoAuth/src/app.js
+++ packages/yonode-templates/JS-MongoDB-TypeORM-NoAuth/src/app.js
@@ -1,13 +1,15 @@
 // import the packages
 import express from "express";
 import { port } from "./config/initial.config.js";
-import AppDataSource from "./config/db.config.js";
 
+//import your files
+import "./config/db.config.js";
+
+//Initializing your app
 const app = express();
-const PORT = port;
 
 // rest of your code here
 
-app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT} 🚀`);
+app.listen(port, () => {
+  console.log(`${chalk.green.bold("Server")} is listening on port ${port}`);
 });
