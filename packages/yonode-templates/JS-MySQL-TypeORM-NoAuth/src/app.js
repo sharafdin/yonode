7802aeb70fb5241d051c2a1435796f90e9567ea2
abdinasir-Tman <abdinasirmursal2@gmail.com>
2024-02-08 19:50:34 -0800

    changed cofig db.config and app.js

--- packages/yonode-templates/JS-MySQL-TypeORM-NoAuth/src/app.js
+++ packages/yonode-templates/JS-MySQL-TypeORM-NoAuth/src/app.js
@@ -4,10 +4,9 @@ import { port } from "./config/initial.config.js";
 import AppDataSource from "./config/db.config.js";
 
 const app = express();
-const PORT = port;
 
 // rest of your code here
 
-app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT} 🚀`);
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
 });
