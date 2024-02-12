f5d822c3612f7927c972d96bef0b1c9f1459c548
Mr Sharafdin <83120892+isasharafdin@users.noreply.github.com>
2024-02-12 12:34:18 +0300

    added prisma client initial.config.js

--- src/config/initial.config.js
+++ src/config/initial.config.js
@@ -1,5 +1,8 @@
 import dotenv from 'dotenv';
+import { PrismaClient } from '@prisma/client';
 
 dotenv.config();
 
 export const port = process.env.SERVER_PORT || 8000;
+
+export const prisma = new PrismaClient();
