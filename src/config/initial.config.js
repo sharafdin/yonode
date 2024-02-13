58decdc9ea1c4bea3cd71165501f3f91db9d0694
Abdinasir Mursal <115280340+abdinasir-Tman@users.noreply.github.com>
2024-02-13 01:19:41 -0800

    Update initial.config.js removed prisma client

--- src/config/initial.config.js
+++ src/config/initial.config.js
@@ -1,8 +1,7 @@
 import dotenv from 'dotenv';
-import { PrismaClient } from '@prisma/client';
 
 dotenv.config();
 
 export const port = process.env.SERVER_PORT || 8000;
 
-export const prisma = new PrismaClient();
+
