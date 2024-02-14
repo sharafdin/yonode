9f9697687f139487e8c7b9b7cf1b2b4145d4c38a
Abdikafi Isse Isak <126691024+Miirshe@users.noreply.github.com>
2024-02-14 17:16:13 +0300

    Fix issue with express.json() middleware not parsing JSON data correctly
    This commit resolves the problem described in the GitHub issue titled "Problem with express.json() middleware not parsing JSON data correctly". The issue was caused by incorrect configuration of express.json(). This commit fixes the problem by updating the configuration to properly handle JSON data parsing.

--- src/app.js
+++ src/app.js
@@ -8,7 +8,8 @@ import "./config/db.config.js";
 
 //Initializing your app
 const app = express();
-app.use(express.json())
+app.use(express.json());
+
 // rest of your code here
 
 app.listen(port, () => {
