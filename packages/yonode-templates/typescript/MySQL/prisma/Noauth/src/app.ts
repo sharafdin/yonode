e7eb6b6ddbb7cd98c687ea0c303e224b17ced790
Omar Jibirl Abdullkadir <129175570+Omar-Tood@users.noreply.github.com>
2024-02-19 22:55:57 +0300

    initializing typescript with Prisma

---
+++ packages/yonode-templates/typescript/MySQL/prisma/Noauth/src/app.ts
@@ -0,0 +1,20 @@
+// import the packages
+import express, { Express } from "express";
+
+// import your files
+import { port } from "./config/initial.config";
+
+
+// initializing the app
+const app: Express = express();
+app.use(express.json());
+
+// rest of your code here
+
+
+
+
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:/${port}/`);
+});
