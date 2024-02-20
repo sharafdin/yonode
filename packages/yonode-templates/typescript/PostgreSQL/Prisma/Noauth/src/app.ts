3c9de2774dc509590a616bc9cb9ba1db1a78a50f
Abdinasir Mursal <abdinasirmursal2@gmail.com>
2024-02-20 11:00:28 -0800

    feat: completed ts-postgresql prisma noAuth api template
    Co-Authored-By: Mr Sharafdin <83120892+isasharafdin@users.noreply.github.com>

--- packages/yonode-templates/typescript/PostgreSQL/Prisma/Noauth/src/app.ts
+++ packages/yonode-templates/typescript/PostgreSQL/Prisma/Noauth/src/app.ts
@@ -2,17 +2,11 @@
 import express, { Express } from "express";
 import { port } from "config/initial.config";
 
-// import your files
-
-
 // initializing the app
 const app: Express = express();
 app.use(express.json());
 
 // rest of your code here
-
-
-
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:/${port}/`);
