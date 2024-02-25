db809404164c090862138850082a457a454dfd03
root <root@linux.sharafdin.com>
2024-02-25 23:09:07 +0300

    feature/typescript: Discontinue development for version 1.0 release

--- packages/yonode/src/lib/repoConditions.js
+++ packages/yonode/src/lib/repoConditions.js
@@ -60,58 +60,58 @@ export const repoConditions = () => {
     // TypeScript
 
     // MongoDB
-    case options.language_type === "TypeScript" &&
-      options.database_type === "MongoDB" &&
-      options.orm_type === "Mongoose" &&
-      options.auth === false:
-      cloneRepo(projectName, "TS-MongoDB-Mongoose-NoAuth-Template");
-      break;
-    case options.language_type === "TypeScript" &&
-      options.database_type === "MongoDB" &&
-      options.orm_type === "Prisma" &&
-      options.auth === false:
-      cloneRepo(projectName, "TS-MongoDB-Prisma-NoAuth-Template");
-      break;
-    case options.language_type === "TypeScript" &&
-      options.database_type === "MongoDB" &&
-      options.orm_type === "TypeORM" &&
-      options.auth === false:
-      cloneRepo(projectName, "TS-MongoDB-TypeORM-NoAuth-Template");
-      break;
-    // MySQL
-    case options.language_type === "TypeScript" &&
-      options.database_type === "MySQL" &&
-      options.orm_type === "Prisma" &&
-      options.auth === false:
-      cloneRepo(projectName, "TS-MySQL-Prisma-NoAuth-Template");
-    case options.language_type === "TypeScript" &&
-      options.database_type === "MySQL" &&
-      options.orm_type === "Sequelize" &&
-      options.auth === false:
-      cloneRepo(projectName, "TS-MySQL-Sequelize-NoAuth-Template");
-    case options.language_type === "TypeScript" &&
-      options.database_type === "MySQL" &&
-      options.orm_type === "TypeORM" &&
-      options.auth === false:
-      cloneRepo(projectName, "TS-MySQL-TypeORM-NoAuth-Template");
-      break;
-    // PostgreSQL
-    case options.language_type === "TypeScript" &&
-      options.database_type === "PostgreSQL" &&
-      options.orm_type === "Prisma" &&
-      options.auth === false:
-      cloneRepo(projectName, "TS-PostgreSQL-Prisma-NoAuth-Template");
-    case options.language_type === "TypeScript" &&
-      options.database_type === "PostgreSQL" &&
-      options.orm_type === "Sequelize" &&
-      options.auth === false:
-      cloneRepo(projectName, "TS-PostgreSQL-Sequelize-NoAuth-Template");
-    case options.language_type === "TypeScript" &&
-      options.database_type === "PostgreSQL" &&
-      options.orm_type === "TypeORM" &&
-      options.auth === false:
-      cloneRepo(projectName, "TS-PostgreSQL-TypeORM-NoAuth-Template");
-      break;
+    // case options.language_type === "TypeScript" &&
+    //   options.database_type === "MongoDB" &&
+    //   options.orm_type === "Mongoose" &&
+    //   options.auth === false:
+    //   cloneRepo(projectName, "TS-MongoDB-Mongoose-NoAuth-Template");
+    //   break;
+    // case options.language_type === "TypeScript" &&
+    //   options.database_type === "MongoDB" &&
+    //   options.orm_type === "Prisma" &&
+    //   options.auth === false:
+    //   cloneRepo(projectName, "TS-MongoDB-Prisma-NoAuth-Template");
+    //   break;
+    // case options.language_type === "TypeScript" &&
+    //   options.database_type === "MongoDB" &&
+    //   options.orm_type === "TypeORM" &&
+    //   options.auth === false:
+    //   cloneRepo(projectName, "TS-MongoDB-TypeORM-NoAuth-Template");
+    //   break;
+    // // MySQL
+    // case options.language_type === "TypeScript" &&
+    //   options.database_type === "MySQL" &&
+    //   options.orm_type === "Prisma" &&
+    //   options.auth === false:
+    //   cloneRepo(projectName, "TS-MySQL-Prisma-NoAuth-Template");
+    // case options.language_type === "TypeScript" &&
+    //   options.database_type === "MySQL" &&
+    //   options.orm_type === "Sequelize" &&
+    //   options.auth === false:
+    //   cloneRepo(projectName, "TS-MySQL-Sequelize-NoAuth-Template");
+    // case options.language_type === "TypeScript" &&
+    //   options.database_type === "MySQL" &&
+    //   options.orm_type === "TypeORM" &&
+    //   options.auth === false:
+    //   cloneRepo(projectName, "TS-MySQL-TypeORM-NoAuth-Template");
+    //   break;
+    // // PostgreSQL
+    // case options.language_type === "TypeScript" &&
+    //   options.database_type === "PostgreSQL" &&
+    //   options.orm_type === "Prisma" &&
+    //   options.auth === false:
+    //   cloneRepo(projectName, "TS-PostgreSQL-Prisma-NoAuth-Template");
+    // case options.language_type === "TypeScript" &&
+    //   options.database_type === "PostgreSQL" &&
+    //   options.orm_type === "Sequelize" &&
+    //   options.auth === false:
+    //   cloneRepo(projectName, "TS-PostgreSQL-Sequelize-NoAuth-Template");
+    // case options.language_type === "TypeScript" &&
+    //   options.database_type === "PostgreSQL" &&
+    //   options.orm_type === "TypeORM" &&
+    //   options.auth === false:
+    //   cloneRepo(projectName, "TS-PostgreSQL-TypeORM-NoAuth-Template");
+    //   break;
 
     default:
       throw new Error("unsupported option");
