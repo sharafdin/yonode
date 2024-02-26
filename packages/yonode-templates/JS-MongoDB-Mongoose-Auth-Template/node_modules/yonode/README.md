2bb7aa14ad4ba742cd400a2fd69871b076b776bf
mchamoudadev <mchamuuda@gmail.com>
2024-02-27 00:49:36 +0300

    added js express with auth

---
+++ packages/yonode-templates/JS-MongoDB-Mongoose-Auth-Template/node_modules/yonode/README.md
@@ -0,0 +1,26 @@
+# Yonode
+
+The Node.js Toolkit for Rapid Development
+
+## Getting Started
+
+Visit our documentation to [get started with Yonode](https://docs.yonode.org).
+
+## Community
+
+Join the vibrant Yonode community:
+
+- **GitHub Discussions**: Engage in conversations, ask questions, share ideas, and showcase your projects.
+- **Discord**: Connect and chat with fellow Yonode users and contributors. [Join](https://discord.gg/GFUK9rS9)
+
+## Contributing
+
+Interested in contributing to Yonode? Check out our [contributing guidelines](CONTRIBUTING.md).
+
+## License
+
+Yonode is an open-source package licensed under the [MIT License](LICENSE)
+
+## Security
+
+[Reporting a Vulnerability](SECURITY.md)
---
+++ packages/yonode-templates/JS-MongoDB-Mongoose-Auth-Template/node_modules/yonode/src/lib/questions.js
@@ -0,0 +1,45 @@
+export const languageType = {
+    type: "list",
+    name: "language_type",
+    message: "Which language do you prefer?",
+    choices: ["JavaScript", "TypeScript"],
+    default: "JavaScript",
+}
+
+export const databaseQuestion = [
+    {
+        type: "list",
+        name: "database_type",
+        message: "Which database do you prefer?",
+        choices: ["MongoDB", "MySQL", "PostgreSQL"],
+        default: "MongoDB",
+    },
+]
+
+export const mongodbOrmQuestion = [
+    {
+        type: "list",
+        name: "mongodb_orm",
+        message: "Which ORM do you prefer?",
+        choices: ["Mongoose", "Prisma", "TypeORM"],
+        default: "Mongoose",
+    },
+]
+
+export const ormQuestion = [
+    {
+        type: "list",
+        name: "orm_type",
+        message: "Which ORM do you prefer?",
+        choices: ["Sequelize", "Prisma", "TypeORM"],
+        default: "Prisma",
+    },
+]
+
+export const authQuestion = [
+    {
+        type: "confirm",
+        name: "auth_type",
+        message: "Do you prefer auth?",
+    },
+]
