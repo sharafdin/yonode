ae117b50a7570ad9189794d0402e0c90ed261628
Abdinasir Mursal <abdinasirmursal2@gmail.com>
2024-03-02 09:17:11 -0800

    init: initilized js-postgresql-auth-template

---
+++ packages/yonode-templates/JS-PostgreSQL-TypeORM-Auth-Template/src/config/dbConfig.js
@@ -0,0 +1,23 @@
+import "reflect-metadata";
+import { DataSource } from "typeorm";
+import { dbName, dbUrl } from "./initialConfig.js";
+import chalk from "chalk";
+import User from "../entity/User.js";
+
+const AppDataSource = new DataSource({
+  type: "mysql",
+  url: dbUrl,
+  database: dbName,
+  entities: [User],
+  synchronize: true,
+});
+
+AppDataSource.initialize()
+  .then(async () => {
+    console.log(`${chalk.green.bold("Connected")} to the database`);
+  })
+  .catch((error) =>
+    console.log(`${chalk.red.bold("Error")} connecting to database`, error)
+  );
+
+export default AppDataSource;
