53d187acfa8d423f4e2554a0c31d8355bcef8dcb
Hanad Mohamed <hanaddaahir124@gmail.com>
2024-03-04 19:32:13 +0300

    Updated Auth Middleware.

--- packages/yonode-templates/JS-MySQL-Sequelize-Auth-Template/src/middlewares/authMiddleware.js
+++ packages/yonode-templates/JS-MySQL-Sequelize-Auth-Template/src/middlewares/authMiddleware.js
@@ -1,9 +1,11 @@
 import jwt from "jsonwebtoken";
+import { jwtSecret } from "../config/initialConfig.js";
 
 // Middleware to validate JWT tokens
-export function auth(req, res, next) {
+export default function auth(req, res, next) {
   // Extract the token from the Authorization header
   const token = req.header("Authorization").replace("Bearer ", "");
+
   // Deny access if the token is not provided
   if (!token) {
     return res.status(401).json({ message: "No token, authorization denied" });
@@ -11,7 +13,7 @@ export function auth(req, res, next) {
 
   try {
     // Verify the token using the secret key
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const decoded = jwt.verify(token, jwtSecret);
     req.user = decoded.userId; // Attach the user ID to the request object
     next(); // Proceed to the next middleware or route handler
   } catch (error) {
