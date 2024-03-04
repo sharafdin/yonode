d197bddcf7eaf2f6179cf26ce10751086a5dcf62
Hanad Mohamed <hanaddaahir124@gmail.com>
2024-03-04 19:43:27 +0300

    Updated InitailConfig file.

---
+++ packages/yonode-templates/JS-MySQL-Sequelize-Auth-Template/src/config/initailConfig.js
@@ -0,0 +1,8 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export const port = process.env.SERVER_PORT || 8000;
+export const dbUrl = process.env.DATABASE_URL + process.env.DATABASE_NAME || "yonodeDB";
+export const jwtSecret = process.env.JWT_SECRET_KEY;
+export const nodeEnv = process.env.NODE_ENVIRONMENT || 'development';
