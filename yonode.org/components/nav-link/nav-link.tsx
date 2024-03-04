d7f9ef011973d3817ab6ab7f1fc801ff768cb6d4
Mr Sharafdin <isasharafdin@gmail.com>
2024-03-04 04:14:26 +0300

    added yonode.org site

---
+++ yonode.org/components/nav-link/nav-link.tsx
@@ -0,0 +1,30 @@
+import { forwardRef } from '@chakra-ui/react'
+
+import { Button, ButtonProps } from '@saas-ui/react'
+
+import Link from 'next/link'
+
+export interface NavLinkProps extends ButtonProps {
+  isActive?: boolean
+  href?: string
+  id?: string
+}
+
+export const NavLink = forwardRef<NavLinkProps, 'a'>((props, ref) => {
+  const { href, type, isActive, ...rest } = props
+
+  return (
+    <Button
+      as={Link}
+      href={href}
+      ref={ref}
+      variant="nav-link"
+      lineHeight="2rem"
+      isActive={isActive}
+      fontWeight="medium"
+      {...rest}
+    />
+  )
+})
+
+NavLink.displayName = 'NavLink'
