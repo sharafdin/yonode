01f7e937afae7c8156708da3cc0c070cbf08c287
Abdinasir Mursal <abdinasirmursal2@gmail.com>
2024-03-06 04:24:22 -0800

    feat: corrected app js-mongo-prisma-auth-template

--- packages/yonode-templates/JS-MongoDB-Prisma-Auth-Template/src/config/initialConfig.js
+++ packages/yonode-templates/JS-MongoDB-Prisma-Auth-Template/src/config/initialConfig.js
@@ -6,3 +6,4 @@ export const port = process.env.SERVER_PORT || 8000;
 export const dbUrl = process.env.DATABASE_URL;
 export const dbName = process.env.DATABASE_NAME || "yonodeDB";
 export const jwtSecret = process.env.JWT_SECRET_KEY;
+export const nodeEnv = process.env.NODE_ENVIRONMENT;
