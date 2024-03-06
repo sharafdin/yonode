4147f1c39fd4acecae24f00216afe1cd4070db7d
Abdinasir Mursal <abdinasirmursal2@gmail.com>
2024-03-06 04:15:29 -0800

    feat: updated authController

--- packages/yonode-templates/JS-MongoDB-Prisma-Auth-Template/src/config/initialConfig.js
+++ packages/yonode-templates/JS-MongoDB-Prisma-Auth-Template/src/config/initialConfig.js
@@ -1,7 +1,8 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 
 dotenv.config();
 
 export const port = process.env.SERVER_PORT || 8000;
 export const dbUrl = process.env.DATABASE_URL;
-export const dbName = process.env.DATABASE_NAME  || 'yonodeDB';
+export const dbName = process.env.DATABASE_NAME || "yonodeDB";
+export const jwtSecret = process.env.JWT_SECRET_KEY;
