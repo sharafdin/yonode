bcdcf9ff00ca9c0dba0895e634438aa76137acfe
Abdinasir Mursal <abdinasirmursal2@gmail.com>
2024-03-07 00:14:32 -0800

    init: JS-MySQL-TypeORM-Auth-Template

--- yonode.org/components/motion/box.tsx
+++
@@ -1,8 +0,0 @@
-import { chakra, ChakraProps, HTMLChakraProps } from '@chakra-ui/react'
-import { motion, HTMLMotionProps } from 'framer-motion'
-
-export interface MotionBoxProps
-  extends HTMLMotionProps<'div'>,
-    Omit<ChakraProps, 'transition' | 'color'> {}
-
-export const MotionBox = motion(chakra.div)
