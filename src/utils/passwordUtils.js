ef46ef933bbc5f09a1e69adf14a75e21ee60d0b6
Abdinasir Mursal <115280340+abdinasir-Tman@users.noreply.github.com>
2024-03-09 04:09:42 -0800

    Update passwordUtils.js

--- src/utils/passwordUtils.js
+++ src/utils/passwordUtils.js
@@ -1,6 +1,6 @@
 import bcrypt from "bcrypt";
 
-export const hashedPassword = async (password) => {
+export const hashPassword = async (password) => {
   const salt = await bcrypt.genSalt(10);
   const hashedPassword = await bcrypt.hash(password, salt);
   return hashedPassword;
