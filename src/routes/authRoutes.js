0a783f292a00aa556b6a3c1adb2469e3400a7bd5
Abdinasir Mursal <abdinasirmursal2@gmail.com>
2024-03-10 10:21:42 -0700

    feat: adjusted the branche

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,36 +1,16 @@
-import express from 'express';
-import { body, validationResult } from 'express-validator';
-import { register, login, logout } from '../controllers/authController.js';
+// Import required modules and configuration
+import express from "express";
+import { loginUser, registerUser } from "../controllers/authController.js";
+import { validateUserLogin, validateUserRegister } from "../middlewares/validators/authValidator.js";
 
+// Create a new router instance
 const router = express.Router();
 
-// Validate registration input
-router.post('/register', [
-    body('email')
-        .isEmail().withMessage('Invalid email')
-        .normalizeEmail(), // This takes care of converting the email to lowercase among other things
-    body('password').isLength({ min: 5 }).withMessage('Password must be at least 5 characters long')],
-    (req, res, next) => {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(400).json({ errors: errors.array() });
-        }
-        next();
-    }, register);
+// Register route with input validation followed by the registration controller
+router.post("/register", validateUserRegister, registerUser);
 
-// Validate login input
-router.post('/login', [
-    body('email')
-        .isEmail().withMessage('Invalid email')
-        .normalizeEmail(), // Normalizes the email to lowercase
-    body('password').exists().withMessage('Password is required'),
-], (req, res, next) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
-    }
-    next();
-}, login);
-router.get('/logout', logout);
+// Login route with input validation followed by the login controller
+router.post("/login", validateUserLogin, loginUser);
 
+// Export the router for use in the main application file
 export default router;
