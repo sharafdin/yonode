93d53e6a07f8c00bcfaf94c85c668287114fb6f1
Abdinasir Mursal <abdinasirmursal2@gmail.com>
2024-03-10 09:14:18 -0700

    feat: renamed hashed to hashedPassword

--- packages/yonode-templates/JS-MySQL-Sequelize-Auth-Template/src/controllers/authController.js
+++ packages/yonode-templates/JS-MySQL-Sequelize-Auth-Template/src/controllers/authController.js
@@ -13,9 +13,9 @@ export async function registerUser(req, res) {
       return res.status(400).json({ message: "User already exists" });
     }
     // Hash the password before saving it
-    const hashed = await hashPassword(password);
+    const hashedPassword = await hashPassword(password);
     // Create a new user builder and save it to the database
-    user = User.build({ email, password:hashed });
+    user = User.build({ email, password: hashedPassword });
     await user.save();
     res.status(201).json({ message: "User created successfully" });
   } catch (error) {
@@ -40,7 +40,7 @@ export async function loginUser(req, res) {
     if (!isMatch) {
       return res.status(400).json({ message: "Invalid Credentials" });
     }
-    
+
     // Create a JWT payload and generate a token
     const payload = { userId: user._id };
     const token = jwt.sign(payload, jwtSecret, {
