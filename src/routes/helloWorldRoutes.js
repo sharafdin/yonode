977c623033ff11c04e80f0e416f131d764294cdb
abdinasir-Tman <abdinasirmursal2@gmail.com>
2024-05-13 18:13:40 +0100

    feat: changed JS-MySQL-TypeORM-NoAuth-Template to the template

---
+++ src/routes/helloWorldRoutes.js
@@ -0,0 +1,8 @@
+import express from 'express'
+import { helloWorld } from '../controllers/helloWorldController.js'
+
+const helloWorldRouter = express.Router()
+
+helloWorldRouter.get("/", helloWorld)
+
+export default helloWorldRouter;
