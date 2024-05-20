dceb6c82e6e7f32f19575400b94ac9af7029963c
Mohamad Al-khatib <alkhatib.m804@gmail.com>
2024-05-21 00:54:32 +0200

    Fix JS-MySQL-Prisma-NoAuth-Template hello-world route

--- packages/yonode-templates/JS-MySQL-Prisma-NoAuth-Template/src/controllers/helloWorldController.js
+++ packages/yonode-templates/JS-MySQL-Prisma-NoAuth-Template/src/controllers/helloWorldController.js
@@ -1,11 +1,10 @@
-export const hellWorld = async (res, req)=>{
-    try{
-
-     res.send("Hello world!")
-
-    }catch(err){
-      res.status(500).json({
-        message: "Internal Server Error"
-      })
-    }
+export const hellWorld = async (req, res)=>{
+  try{
+    res.send("Hello world!")
+  }
+  catch(err){
+    res.status(500).json({
+      message: "Internal Server Error"
+    })
+  }
 }
