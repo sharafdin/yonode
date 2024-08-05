e2eb00b6d92cfb025e5dfd47d41b8de7ac36a7a7
Mohamad Al-khatib <alkhatib.m804@gmail.com>
2024-08-05 02:05:30 +0200

    up

--- packages/JS-MongoDB-Prisma-NoAuth-Template/src/controllers/helloWorldController.js
+++
@@ -1,9 +0,0 @@
-export const helloWorld = async (req, res) => {
-    try {
-      res.send("Hello World!");
-    } catch (error) {
-      res.status(500).json({
-        message: "Internal Server Error",
-      });
-    }
-  };
